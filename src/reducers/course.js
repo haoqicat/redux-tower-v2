16bf8d9a244ba5ed8459e46455f98ea74ccdc5ba
Peter Wang <happypeter1983@gmail.com>
2018-01-17 21:49:56 +0800

    like reducer

--- src/reducers/course.js
+++ src/reducers/course.js
@@ -1,20 +1,31 @@
 import { combineReducers } from 'redux'
+import * as types from '../constants/ActionTypes'
 
 const initialState = [
   {
     id: '1',
     title: '课程1',
-    likes: '2'
+    likes: 2
   },
   {
     id: '2',
     title: '课程2',
-    likes: '3'
+    likes: 3
   }
 ]
 
 const all = (state = initialState, action) => {
   switch (action.type) {
+    case types.LIKE:
+      return state.map(t => {
+        if (t.id === action.id) {
+          return {
+            ...t,
+            likes: t.likes + 1
+          }
+        }
+        return t
+      })
     default:
       return state
   }
