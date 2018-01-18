967900aa625c553123c100cf59871632676fabaf
Peter Wang <happypeter1983@gmail.com>
2018-01-18 10:22:47 +0800

    controlled

--- src/components/CourseCommentForm.js
+++ src/components/CourseCommentForm.js
@@ -4,16 +4,54 @@ import Input from 'material-ui/Input'
 import Button from 'material-ui/Button'
 
 class CommentForm extends Component {
+  state = {
+    body: '',
+    user: ''
+  }
+
+  bodyChange = e => {
+    e.preventDefault()
+    this.setState({
+      body: e.target.value
+    })
+  }
+
+  userChange = e => {
+    e.preventDefault()
+    this.setState({
+      user: e.target.value
+    })
+  }
+
+  handleClick = e=> {
+    e.preventDefault()
+    const { body, user } = this.state
+    const comment = {
+      body,
+      user
+    }
+    console.log('comment', comment)
+    this.setState({
+      body: '',
+      user: ''
+    })
+  }
   render() {
+    const { user, body } = this.state
     return (
       <Wrap>
         <NameInput
+          value={user}
+          onChange={this.userChange}
           placeholder="名字"
         />
         <CommentInput
+          value={body}
+          onChange={this.bodyChange}
           placeholder="评论"
         />
-        <Button raised color="accent" >
+        <Button onClick={this.handleClick}
+          raised color="accent" >
           评论
         </Button>
       </Wrap>
